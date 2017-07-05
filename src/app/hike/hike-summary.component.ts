c1455d5b395b8611a031b6bd8bf2088399dd8794
mauger <auger.mickael37@gmail.com>
2017-07-05 15:07:37 +0200

    second commit

---
+++ src/app/hike/hike-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+import { Hike } from './hike';
+
+@Component({
+    moduleId: module.id,
+    selector: 'hike-summary',
+    templateUrl: 'hike-summary.component.html'
+})
+export class HikeSummaryComponent {
+    @Input() hike: Hike;
+}
--- src/app/hike/hike.module.ts
+++ src/app/hike/hike.module.ts
@@ -7,11 +7,12 @@ import { HikeDetailsComponent } from './hike-details.component';
 import { HikeFilterPipe } from './hike-filter.pipe';
 import { HikeListComponent } from './hike-list.component';
 import { HikeService } from './hike.service';
+import { HikeSummaryComponent} from './hike-summary.component';
 
 @NgModule({
     imports:      [CommonModule, RouterModule, FormsModule],
     exports:      [HikeListComponent],
-    declarations: [HikeListComponent, HikeDetailsComponent, HikeFilterPipe],
+    declarations: [HikeListComponent, HikeDetailsComponent, HikeFilterPipe, HikeSummaryComponent],
     providers:    [HikeService]
 })
 export class HikeModule { }
